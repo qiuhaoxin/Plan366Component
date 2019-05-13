2ced97acb927b6ec636a284827b68ac7235372d5
qiuhaoxin <502967244@qq.com>
2019-05-13 17:44:53 +0800

    commit

---
+++ .babelrc.js
@@ -0,0 +1,20 @@
+
+
+
+module.exports={
+	presets:[
+       [
+          '@babel/env',
+          targets:{
+          	 chrome:'67',
+          	 safari:'11.1',
+          	 firefox:'60',
+          	 edge:'17',
+          },
+       ],
+       '@babel/react',
+	],
+	plugins:[
+
+	]
+}
